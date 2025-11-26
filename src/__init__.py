c57cde4e4a5d07477ace922945b2ccaa33f90b30
Aaryan Kurade <aaryankurade27@gmail.com>
2025-11-26 15:58:56 +0530

    Downloaded and converted soccernet tracking MOT format into YOLO format

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+# SportsVision+ Source Package
+"""
+End-to-end real-time football analytics system.
+"""
+
+__version__ = "0.1.0"
---
+++ src/utils/__init__.py
@@ -0,0 +1,7 @@
+# Utilities Layer
+"""
+Geometry (Homography/ViewTransformer) and Visualization utilities.
+"""
+
+from .geometry import ViewTransformer
+from .viz import draw_pitch, draw_points_on_pitch, SoccerPitchConfiguration
